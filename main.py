e50b9bcfeceed9ae0586623e344379ebd5373239
DarkFighterLuke <luca-consoli@live.it>
2024-05-14 14:47:52 +0200

    Add logging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import telebot
@@ -7,15 +8,18 @@ from openai import OpenAI
 load_dotenv()
 bot = telebot.TeleBot(token=os.getenv("BOT_TOKEN"))
 client = OpenAI(api_key=os.getenv("OPENAI_TOKEN"))
+logger = logging.getLogger("[GPTEmotionsBot]")
+logging.basicConfig(level=logging.INFO)
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    logger.info(f"User {message.from_user.username} sent /start command")
     bot.reply_to(message, "Benvenuto nel bot di GPTEmotions!\nScrivi una frase da analizzare per ottenere l'emozione "
                           "rilevata :)")
 
 
-def gpt_chat(user_input : str):
+def gpt_chat(user_input: str):
     completion = client.chat.completions.create(
         model="ft:gpt-3.5-turbo-0125:personal:emotions:9MFmXPJ9",
         messages=[
@@ -34,6 +38,7 @@ def parse_query(message_text):
 
 @bot.message_handler(func=lambda message: message.text[0] != "/")
 def analyze_sentiment(message):
+    logger.info(f"User {message.from_user.username} sent text: {message.text}")
     sentiment = gpt_chat(message.text)
     bot.reply_to(message, sentiment)
 
@@ -41,6 +46,7 @@ def analyze_sentiment(message):
 @bot.message_handler(commands=["analyze"])
 def analyze_sentiment_by_command(message):
     text = parse_query(message.text)
+    logger.info(f"User {message.from_user.username} sent /analyze command with text '{text}'")
     sentiment = gpt_chat(text)
     bot.reply_to(message, sentiment)
 
